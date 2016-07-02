041e87854761c7cf96c1e1179427ee9672e06ea4
silverweed <silverweed1991@gmail.com>
2016-07-02 11:58:12 +0200

    add frontend + run script

---
+++ run.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+ROOT="$(readlink -f $(dirname $0))"
+EXE="$ROOT/dist/build/maudstats/maudstats"
+
+[[ -x $(which cabal 2>/dev/null) ]] || {
+	>&2 echo "[ FATAL ] Missing cabal executable!"
+	exit 1
+}
+[[ -x $(which awk 2>/dev/null) ]] || {
+	>&2 echo "[ FATAL ] Missing awk executable!"
+	exit 2
+}
+[[ -x $EXE ]] || {
+	cabal install
+	[[ $? == 0 ]] || {
+		>&2 echo "[ FATAL ] Errors building maudstats!"
+		exit 3
+	}
+}
+$EXE | awk > $ROOT/frontend/data.js '
+{
+	++i
+	labels[i] = $1
+	data[i] = $2
+}
+
+END {
+	printf "\"use strict\";\r\nvar ext = {\r\n\tlabels: ["
+	for (j = 1; j < i - 1; ++j) 
+		printf labels[j]", "
+	printf labels[i-1]"],\r\n\tdata: ["
+	for (j = 1; j < i - 1; ++j)
+		printf data[j]", "
+	printf data[i-1]"]\r\n};"
+}'
