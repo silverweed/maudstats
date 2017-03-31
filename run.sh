e6d1a8a811569b56dc369a0e7a15c366a4c957b8
silverweed <silverweed1991@gmail.com>
2017-03-31 15:55:51 +0200

    update frontend

--- run.sh
+++ run.sh
@@ -7,10 +7,10 @@ EXE="$ROOT/dist/build/maudstats/maudstats"
 	>&2 echo "[ FATAL ] Missing cabal executable!"
 	exit 1
 }
-[[ -x $(which awk 2>/dev/null) ]] || {
-	>&2 echo "[ FATAL ] Missing awk executable!"
-	exit 2
-}
+#[[ -x $(which awk 2>/dev/null) ]] || {
+	#>&2 echo "[ FATAL ] Missing awk executable!"
+	#exit 2
+#}
 [[ -x $EXE ]] || {
 	cabal install
 	[[ $? == 0 ]] || {
@@ -18,19 +18,20 @@ EXE="$ROOT/dist/build/maudstats/maudstats"
 		exit 3
 	}
 }
-$EXE | awk -v 'FS=|' > $ROOT/frontend/data.js '
-{
-	++i
-	labels[i] = $1
-	data[i] = $2
-}
+$EXE > $ROOT/frontend/data.js
+#| awk -v 'FS=|' > $ROOT/frontend/data.js '
+#{
+	#++i
+	#labels[i] = $1
+	#data[i] = $2
+#}
 
-END {
-	printf "\"use strict\";\r\nvar ext = {\r\n\tlabels: ["
-	for (j = 1; j < i - 1; ++j) 
-		printf "\""labels[j]"\", "
-	printf "\""labels[i]"\"],\r\n\tdata: ["
-	for (j = 1; j < i - 1; ++j)
-		printf data[j]", "
-	printf data[i]"]\r\n};"
-}'
+#END {
+	#printf "\"use strict\";\r\nvar ext = {\r\n\tlabels: ["
+	#for (j = 1; j < i - 1; ++j) 
+		#printf "\""labels[j]"\", "
+	#printf "\""labels[i]"\"],\r\n\tdata: ["
+	#for (j = 1; j < i - 1; ++j)
+		#printf data[j]", "
+	#printf data[i]"]\r\n};"
+#}'
